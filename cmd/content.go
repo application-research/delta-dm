553620b314832074046a52922be5e3293298541c
Jason Cihelka <jcihelka@isotechnics.com>
2023-04-03 09:25:17 -0700

    feat: firm up content cli flags

--- cmd/content.go
+++ cmd/content.go
@@ -1,10 +1,9 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
-	"github.com/application-research/delta-dm/core"
 	"github.com/urfave/cli/v2"
 )
 
@@ -29,11 +28,19 @@ func ContentCmd() []*cli.Command {
 						Required:    true,
 					},
 					&cli.StringFlag{
-						Name:        "dataset",
-						Aliases:     []string{"d"},
-						Usage:       "dataset name (slug)",
-						Destination: &datasetName,
-						Required:    true,
+						Name:    "json",
+						Aliases: []string{"j"},
+						Usage:   "content in ddm json format",
+					},
+					&cli.StringFlag{
+						Name:    "csv",
+						Aliases: []string{"c"},
+						Usage:   "filename of content in csv format",
+					},
+					&cli.StringFlag{
+						Name:    "singularity",
+						Aliases: []string{"s"},
+						Usage:   "content in singularity json export format",
 					},
 				},
 				Action: func(c *cli.Context) error {
@@ -41,20 +48,32 @@ func ContentCmd() []*cli.Command {
 					if err != nil {
 						return err
 					}
+					jsonData := c.String("json")
+					csvFilename := c.String("csv")
+					singularityData := c.String("singularity")
 
-					inputCnt := c.Args().First()
-
-					if inputCnt == "" {
-						return fmt.Errorf("must provide content")
+					if jsonData == "" && csvFilename == "" && singularityData == "" {
+						return fmt.Errorf("must either json, singularity or csv flag")
 					}
 
-					var cnt []core.Content
+					var body []byte
+					url := "/contents/" + datasetName
 
-					if err := json.Unmarshal([]byte(inputCnt), &cnt); err != nil {
-						return fmt.Errorf("couldn't parse content: %s", err)
+					if jsonData != "" {
+						body = []byte(jsonData)
+					} else if csvFilename != "" {
+						csvFile, err := ioutil.ReadFile(csvFilename)
+						if err != nil {
+							return fmt.Errorf("failed to open csv file: %s", err)
+						}
+						body = csvFile
+						url += "?import_type=csv"
+					} else if singularityData != "" {
+						body = []byte(singularityData)
+						url += "?import_type=singularity"
 					}
 
-					res, closer, err := cmd.MakeRequest("POST", fmt.Sprintf("/contents/%s", datasetName), []byte(inputCnt))
+					res, closer, err := cmd.MakeRequest("POST", fmt.Sprintf("/contents/%s", datasetName), body)
 
 					if err != nil {
 						return fmt.Errorf("unable to make request %s", err)
