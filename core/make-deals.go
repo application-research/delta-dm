e3d78efee1bfe1d16412637b9e7014607db833ac
Jason Cihelka <jcihelka@isotechnics.com>
2023-04-27 10:28:00 -0700

    feat: fully mocked dry run mode

--- core/make-deals.go
+++ core/make-deals.go
@@ -15,7 +15,29 @@ func (dldm *DeltaDM) MakeDeals(dealsToMake OfflineDealRequest, authKey string, i
 		fmt.Println(util.Red + "-- DRY RUN MODE (NO DEALS MADE) --" + util.Reset)
 		fmt.Printf("\n\n %+v \n\n", dealsToMake)
 		fmt.Println(util.Red + "---------------------------------" + util.Reset)
-		return &OfflineDealResponse{}, nil
+
+		dealResp, _ := dryRunDeal(&dealsToMake)
+
+		for _, c := range *dealResp {
+			var newReplication = Replication{
+				ContentCommP:    c.DealRequestMeta.PieceCommitment.PieceCid,
+				ProviderActorID: c.DealRequestMeta.Miner,
+				DeltaContentID:  c.DeltaContentID,
+				DealTime:        time.Now(),
+				Status:          StatusSuccess,
+				IsSelfService:   isSelfService,
+				ProposalCid:     "DRY_RUN_" + fmt.Sprint(rand.Int()),
+				DeltaMessage:    "this is a dry run, no deal was made",
+			}
+
+			res := dldm.DB.Model(&Replication{}).Create(&newReplication)
+			if res.Error != nil {
+				log.Errorf("unable to create replication in db: %s", res.Error)
+				continue
+			}
+		}
+
+		return dealResp, nil
 	}
 
 	deltaResp, err := dldm.DAPI.MakeOfflineDeals(dealsToMake, authKey)
@@ -47,4 +69,21 @@ func (dldm *DeltaDM) MakeDeals(dealsToMake OfflineDealRequest, authKey string, i
 		dldm.DB.Model(&Content{}).Where("comm_p = ?", newReplication.ContentCommP).Update("num_replications", gorm.Expr("num_replications + ?", 1))
 	}
 	return deltaResp, nil
+}
+
+// Stub function to generate a mocked deal response for local testing.
+func dryRunDeal(odr *OfflineDealRequest) (*OfflineDealResponse, error) {
+	var resp OfflineDealResponse
+
+	for _, d := range *odr {
+		var newResponse = OfflineDealResponseElement{
+			DealRequestMeta: d,
+			DeltaContentID:  rand.Int63(),
+			Status:          "success",
+		}
+
+		resp = append(resp, newResponse)
+	}
+
+	return &resp, nil
 }
